c70f98dc995acf2526dd3950a554163b778e2a07
Guinea Wheek <guineawheek@gmail.com>
2026-08-03 01:29:12 -0700

    reduxfifo: always build with debuginfo and strip after build
    building both debug + release profiles sucks. let's just always build
    with release but always include debuginfo and strip it after the fact.
    
    also just drop static linking as a concept (please don't statically link
    to reduxfifo)
    
    net result should be tiny vdep zips

--- reduxfifo/xtask/src/main.rs
+++ reduxfifo/xtask/src/main.rs
@@ -1,7 +1,7 @@
 use std::path::{Path, PathBuf};
 
 use clap::Parser as _;
-use maven_utils::{BuildConfig, Target, build_maven_zip, locate_systemcore_toolchain};
+use maven_utils::{Target, build_maven_zip, locate_systemcore_toolchain};
 
 use crate::maven_utils::ReduxFIFOCrate;
 #[cfg(all(target_os = "linux", not(target_arch = "aarch64")))]
@@ -17,15 +17,6 @@ const ARTIFACT_ID: &str = "ReduxLib-fifo";
 struct Cli {
     #[arg(num_args = 1..)]
     targets: Vec<Compileable>,
-    #[arg(long = "static", help = "Compile static instead of shared binaries")]
-    static_build: bool,
-    #[arg(long = "debug", help = "Compile with debug symbols")]
-    debug_build: bool,
-    #[arg(
-        long = "all",
-        help = "Compile all permutations of shared/static and release/debug binaries"
-    )]
-    all_build: bool,
     #[arg(
         long = "workspace-root",
         help = "ReduxFIFO workspace root",
@@ -38,25 +29,6 @@ struct Cli {
         help = "args to pass through to Cargo"
     )]
     cargo_flags: Vec<String>,
-}
-
-impl Cli {
-    fn build_configs(&self) -> Vec<BuildConfig> {
-        if self.all_build {
-            return vec![
-                BuildConfig::Shared,
-                BuildConfig::Static,
-                BuildConfig::SharedDebug,
-                BuildConfig::StaticDebug,
-            ];
-        }
-        vec![match (self.static_build, self.debug_build) {
-            (false, false) => BuildConfig::Shared,
-            (false, true) => BuildConfig::SharedDebug,
-            (true, false) => BuildConfig::Static,
-            (true, true) => BuildConfig::StaticDebug,
-        }]
-    }
 }
 
 #[derive(clap::ValueEnum, Clone, Copy, PartialEq, Eq, Debug, Default)]
@@ -85,53 +57,42 @@ enum Compileable {
 fn main() -> anyhow::Result<()> {
     let cli = Cli::try_parse()?;
     let ci = ReduxFIFOCrate::new(&cli.workspace_root)?;
-    let build_configs = cli.build_configs();
     let cargo_flags = cli.cargo_flags;
     for target in cli.targets {
         match target {
-            Compileable::LinuxX86_64 => {
-                build_maven(&ci, Target::LinuxX86_64, &build_configs, &cargo_flags)?
-            }
-            Compileable::LinuxArm64 => {
-                build_maven(&ci, Target::LinuxArm64, &build_configs, &cargo_flags)?
-            }
+            Compileable::LinuxX86_64 => build_maven(&ci, Target::LinuxX86_64, &cargo_flags)?,
+            Compileable::LinuxArm64 => build_maven(&ci, Target::LinuxArm64, &cargo_flags)?,
             Compileable::LinuxSystemCore => {
-                build_maven(&ci, Target::LinuxSystemCore, &build_configs, &cargo_flags)?
+                build_maven(&ci, Target::LinuxSystemCore, &cargo_flags)?
             }
-            Compileable::WindowsX86_64 => {
-                build_maven(&ci, Target::WindowsX86_64, &build_configs, &cargo_flags)?
-            }
-            Compileable::WindowsArm64 => {
-                build_maven(&ci, Target::WindowsArm64, &build_configs, &cargo_flags)?
-            }
-            Compileable::OsxUniversal => {
-                build_maven(&ci, Target::OsxUniversal, &build_configs, &cargo_flags)?
-            }
+            Compileable::WindowsX86_64 => build_maven(&ci, Target::WindowsX86_64, &cargo_flags)?,
+            Compileable::WindowsArm64 => build_maven(&ci, Target::WindowsArm64, &cargo_flags)?,
+            Compileable::OsxUniversal => build_maven(&ci, Target::OsxUniversal, &cargo_flags)?,
             Compileable::Headers => {
                 build_maven_zip(&ci, Path::new("include"), GROUP_ID, ARTIFACT_ID, "headers")?;
             }
             Compileable::Desktop => {
-                build_maven_desktop(&ci, &build_configs, &cargo_flags)?;
+                build_maven_desktop(&ci, &cargo_flags)?;
             }
             Compileable::Auto => {
                 // always build headers
                 build_maven_zip(&ci, Path::new("include"), GROUP_ID, ARTIFACT_ID, "headers")?;
                 // always build systemcore if possible
                 if locate_systemcore_toolchain(ci.year).is_ok() {
-                    build_maven(&ci, Target::LinuxSystemCore, &build_configs, &cargo_flags)?;
+                    build_maven(&ci, Target::LinuxSystemCore, &cargo_flags)?;
                 }
 
                 // build platform-dependent targets
-                build_maven_desktop(&ci, &build_configs, &cargo_flags)?;
+                build_maven_desktop(&ci, &cargo_flags)?;
 
                 // build extra targets if applicable
                 #[cfg(all(target_os = "linux", not(target_arch = "aarch64")))]
                 if locate_aarch64_toolchain(ci.year).is_ok() {
-                    build_maven(&ci, Target::LinuxArm64, &build_configs, &cargo_flags)?;
+                    build_maven(&ci, Target::LinuxArm64, &cargo_flags)?;
                 }
 
                 #[cfg(all(target_os = "windows", not(target_arch = "aarch64")))]
-                build_maven(&ci, Target::WindowsArm64, &build_configs, &cargo_flags)?
+                build_maven(&ci, Target::WindowsArm64, &cargo_flags)?
             }
         }
     }
@@ -140,7 +101,6 @@ fn main() -> anyhow::Result<()> {
 
 fn build_maven_desktop(
     crate_info: &ReduxFIFOCrate,
-    build_configs: &[BuildConfig],
     cargo_flags: &Vec<String>,
 ) -> anyhow::Result<()> {
     #[cfg(all(target_os = "linux", target_arch = "x86_64"))]
@@ -154,23 +114,15 @@ fn build_maven_desktop(
     #[cfg(all(target_os = "windows", target_arch = "aarch64"))]
     let target = Target::WindowsArm64;
 
-    build_maven(&crate_info, target, &build_configs, &cargo_flags)
+    build_maven(&crate_info, target, &cargo_flags)
 }
 
 fn build_maven(
     crate_info: &ReduxFIFOCrate,
     target: Target,
-    build_configs: &[BuildConfig],
     cargo_flags: &Vec<String>,
 ) -> anyhow::Result<()> {
-    maven_utils::build_maven(
-        crate_info,
-        target,
-        GROUP_ID,
-        ARTIFACT_ID,
-        build_configs,
-        cargo_flags,
-    )?;
+    maven_utils::build_maven(crate_info, target, GROUP_ID, ARTIFACT_ID, cargo_flags)?;
     maven_utils::build_maven_pom(crate_info, GROUP_ID, ARTIFACT_ID)?;
     maven_utils::build_maven_metadata(crate_info, GROUP_ID, ARTIFACT_ID)?;
     Ok(())
